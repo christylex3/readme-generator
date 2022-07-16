17da51eaf2e94d0bb7ff28bd2a9a55c6e5dac625
Christy Le <christylex3@gmail.com>
2022-07-16 13:45:24 -0400

    Added questions into the question array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", "What is the purpose of this project?", "What installation is required for this project?", "How do you use this project?", "What is this project licensed under?", "How do you want others to contribute to the project?", "What are the testing instructions?", "What is your GitHub?", "What is your email address?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
