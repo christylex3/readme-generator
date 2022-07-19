5d72d25d258ce0cbb97758ce2ee86dbb01cd3122
Christy Le <christylex3@gmail.com>
2022-07-18 21:19:00 -0400

    Added renderLicenseSection(data)

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,13 +14,18 @@ function renderLicenseBadge(data) {
   }
 }
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(data) {
+  if (data.license !== "None") {
+    return `## License\nThis application is licensed under ${data.license}.`
+  }
+  return ``;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -33,10 +38,9 @@ function generateMarkdown(data) {
   * [License](#license)\n
   * [Contribution](#contribution)\n
   * [Tests](#tests)\n
-  * [Questions](#questions)\n
-  ## Installation\n${data.installation}\n
+  * [Questions](#questions)\n## Installation\n${data.installation}\n
   ## Usage\n${data.usage}\n
-  ## License\nThis application is licensed under ${data.license}\n
+  ${renderLicenseSection(data)}\n
   ## Contribution\n${data.contribution}\n
   ## Tests\n${data.test}\n
   ## Questions\nIf you have any questions and want to reach me, contact me here at [${data.githubUsername}@github.com](github.com/${data.githubUsername}) or ${data.email}.
