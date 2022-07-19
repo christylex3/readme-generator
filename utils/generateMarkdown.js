47b16c5a35c3e6d7dc99947bded5ae3503d2df8b
Christy Le <christylex3@gmail.com>
2022-07-18 22:19:01 -0400

    Removed additional spaces in generateMarkdown(date)

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,21 +34,7 @@ function renderLicenseSection(data) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}\n
-  ${renderLicenseBadge(data)}
-  ## Description\n${data.description}\n
-  ## Table of Contents\n
-  * [Installation](#installation)\n
-  * [Usage](#usage)\n
-  ${renderLicenseLink(data)}
-  * [Contribution](#contribution)\n
-  * [Tests](#tests)\n
-  * [Questions](#questions)\n## Installation\n${data.installation}\n
-  ## Usage\n${data.usage}\n
-  ${renderLicenseSection(data)}
-  ## Contribution\n${data.contribution}\n
-  ## Tests\n${data.test}\n
-  ## Questions\nIf you have any questions and want to reach me, contact me here at [${data.githubUsername}@github.com](github.com/${data.githubUsername}) or ${data.email}.
+  return `# ${data.title}\n${renderLicenseBadge(data)}## Description\n${data.description}\n## Table of Contents\n* [Installation](#installation)\n* [Usage](#usage)\n${renderLicenseLink(data)}* [Contribution](#contribution)\n* [Tests](#tests)\n* [Questions](#questions)\n## Installation\n${data.installation}\n## Usage\n${data.usage}\n${renderLicenseSection(data)}## Contribution\n${data.contribution}\n## Tests\n${data.test}\n## Questions\nIf you have any questions and want to reach me, contact me here at [${data.githubUsername}@github.com](github.com/${data.githubUsername}) or ${data.email}.
 `;
 }
 
