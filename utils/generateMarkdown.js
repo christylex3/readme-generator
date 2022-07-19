2999964b4486ff7aabd6cc75335c6f71d6e192fb
Christy Le <christylex3@gmail.com>
2022-07-18 21:47:01 -0400

    Added \n to license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,13 +2,13 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
   if (data.license == "Apache 2") {
-    return `![Apache 2](https://img.shields.io/badge/license-Apache%202-blue)`
+    return `![Apache 2](https://img.shields.io/badge/license-Apache%202-blue)\n`
   } else if (data.license == "BSD") {
-    return `![BSD](https://img.shields.io/badge/license-BSD-blue)`;
+    return `![BSD](https://img.shields.io/badge/license-BSD-blue)\n`;
   } else if (data.license == "GPL") {
-    return `![GPL](https://img.shields.io/badge/license-GPL-blue)`;
+    return `![GPL](https://img.shields.io/badge/license-GPL-blue)\n`;
   } else if (data.license == "MIT") {
-    return `![MIT](https://img.shields.io/badge/license-MIT-blue)`;
+    return `![MIT](https://img.shields.io/badge/license-MIT-blue)\n`;
   } else {
     return ``;
   }
@@ -27,7 +27,7 @@ function renderLicenseLink(data) {
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
   if (data.license !== "None") {
-    return `## License\nThis application is licensed under ${data.license}.`
+    return `## License\nThis application is licensed under ${data.license}.\n`
   }
   return ``;
 }
@@ -35,7 +35,7 @@ function renderLicenseSection(data) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}\n
-  ${renderLicenseBadge(data)}\n
+  ${renderLicenseBadge(data)}
   ## Description\n${data.description}\n
   ## Table of Contents\n
   * [Installation](#installation)\n
@@ -45,7 +45,7 @@ function generateMarkdown(data) {
   * [Tests](#tests)\n
   * [Questions](#questions)\n## Installation\n${data.installation}\n
   ## Usage\n${data.usage}\n
-  ${renderLicenseSection(data)}\n
+  ${renderLicenseSection(data)}
   ## Contribution\n${data.contribution}\n
   ## Tests\n${data.test}\n
   ## Questions\nIf you have any questions and want to reach me, contact me here at [${data.githubUsername}@github.com](github.com/${data.githubUsername}) or ${data.email}.
